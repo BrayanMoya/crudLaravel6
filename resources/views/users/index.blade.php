13afd9a8abda8ed74918eb19c71ee7e6930d8f46
bmoya <bmoya17@hotmail.com>
2021-08-18 12:42:50 -0500

    Feat: :bulb: Commit inicial, se crean rutas y controladores para dichas rutas
    Signed-off-by: bmoya <bmoya17@hotmail.com>

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <!-- Styles -->
+</head>
+<body>
+
+</body>
+</html>

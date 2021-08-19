7820fbef3028be139fd4698393441bb5a88ae9d4
bmoya <bmoya17@hotmail.com>
2021-08-19 18:28:53 -0500

    Feat: :bulb: Se finaliza con la introducción

--- app/User.php
+++ app/User.php
@@ -36,4 +36,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function getGetNameAttribute($key)
+    {
+        return strtoupper($this->name);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtolower($value);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use App\Post;
+use App\User;
+
 Route::get('/', function () {
     return view('post');
 });
@@ -26,3 +29,55 @@ Route::post('post', 'PostController@store')->name('posts.store');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('eloquent', function () {
+    $posts = Post::where('id', '>=', '20')
+        ->orderBy('id', 'desc')
+        ->take(5)
+        ->get();
+
+    foreach ($posts as $post) {
+        echo "$post->id $post->title <br/>";
+    }
+});
+
+Route::get('posts', function () {
+    $posts = Post::get();
+
+    foreach ($posts as $post) {
+        echo "{$post->id} <strong>{$post->user->getName}</strong> {$post->getTitle} <br/>";
+    }
+});
+
+Route::get('users', function () {
+    $users = User::all();
+
+    foreach ($users as $user) {
+        $posts = $user->posts;
+
+        foreach ($posts as $post) {
+            echo "{$user->id} <strong>{$post->getTitle}</strong> {$user->getName} <br/>";
+        }
+    }
+});
+
+Route::get('collections', function () {
+    $users = User::all();
+
+//    dd($users);
+//    dd($users->contains(5)); // Valida si contiene 5 elementos
+//    dd($users->except([1,2,3])); // exceptuando elementos, buscados por su llave primaria (id)
+//    dd($users->only([1,4])); // unicamente aquellos que se le pasen por parametro, buscados por su llave primaria (id)
+//    dd($users->find(2)); // busca por su llave primaria aquel que se le pase por parametro o por modelo si se le pasa directamente un modelo por parametro, el resultado no es una colleción, no habria necesidad de iterarlo
+    dd($users->load('posts')); // trae la colleción con su relación que se pàse por parametro (el nombre de la relación que se le pasa por parametro debe de coincidir con el método que se agrego en el modelo para la relación)
+
+});
+
+Route::get('serialization', function () {
+    $users = User::all();
+
+//    dd($users->toArray()); // trae la información en array unicamente con los datos de la BD
+    $user = $users->find(2);
+    dd($user ? $user->toJson() : '');
+});
